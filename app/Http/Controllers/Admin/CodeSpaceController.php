1cb13dc86d1b9fa5af704f9c3e8ad00b095a0e54
1Mike21 <mihail.sokolov21@yandex.ru>
2024-01-10 19:26:40 +0300

    Created -all for CodeSpace

---
+++ app/Http/Controllers/Admin/CodeSpaceController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CodeSpace;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCodeSpaceRequest;
+use App\Http\Requests\UpdateCodeSpaceRequest;
+
+class CodeSpaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCodeSpaceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CodeSpace $codeSpace)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CodeSpace $codeSpace)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCodeSpaceRequest $request, CodeSpace $codeSpace)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CodeSpace $codeSpace)
+    {
+        //
+    }
+}
