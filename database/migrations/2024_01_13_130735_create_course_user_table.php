b29ca2cd9a97580298ee53489ff3aec1a419935b
1Mike21 <mihail.sokolov21@yandex.ru>
2024-01-13 16:56:31 +0300

    Created migration pivot tables course_user

---
+++ database/migrations/2024_01_13_130735_create_course_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('course_id')->nullable()->constrained()->nullOnDelete()->cascadeOnUpdate();
+            $table->boolean('with_mentor')->default(false);
+            $table->boolean('is_passed')->default(false);
+            $table->integer('progress');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_user');
+    }
+};
