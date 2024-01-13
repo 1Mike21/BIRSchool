512d3aea52734586ebba4630cf9cad98eea4a907
1Mike21 <mihail.sokolov21@yandex.ru>
2024-01-13 18:52:27 +0300

    Created -all for GroupResource

---
+++ app/Models/GroupResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class GroupResource extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'title',
+      'text',
+      'img',
+      'group_id'
+    ];
+
+    public function group(): BelongsTo {
+      return $this->belongsTo(Group::class);
+    }
+}
