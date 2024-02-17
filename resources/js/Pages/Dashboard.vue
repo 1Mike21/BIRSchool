291a4ad9835b1806e330296808153acebb7d5d52
1Mike21 <mihail.sokolov21@yandex.ru>
2024-02-17 18:29:15 +0300

    Deleted component and page Welcome

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,16 +1,8 @@
 <template>
   <Head title="Личный кабинет"/>
-  <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-              <Welcome />
-          </div>
-      </div>
-  </div>
 </template>
 
 <script setup>
-  import Welcome from '@/Components/Welcome.vue';
   import { Head } from '@inertiajs/vue3';
 
 </script>
