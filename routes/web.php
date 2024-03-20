f29e2b24dd127ee7e59a034da89b0a6d16949c1e
1Mike21 <mihail.sokolov21@yandex.ru>
2024-03-20 20:51:39 +0300

    Moved the routes Fortify and Jetstream to the application

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,10 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
+]);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
+require_once __DIR__ . '/jetstream.php';
+require_once __DIR__ . '/fortify.php';
