e5704d5f7b6406842c315296df4271cd70c2cee0
1Mike21 <mihail.sokolov21@yandex.ru>
2024-04-17 20:34:30 +0300

    Created sidebar
    (cherry picked from commit 218cdf8dcd81309bfd01e9d3e389e9a83e516856)

---
+++ resources/js/Components/Sidebar/SidebarDropdown.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
+    <template v-for="(childItem) in items" :key="index">
+      <li>
+        <Link
+          :active="route().current(item.route)"
+          :href="route(item.route) "
+          class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+        >
+        {{ childItem.label }}
+        </Link>
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+  items: {
+    type: Array,
+    default: () => []
+  }
+});
+const items = ref(props.items);
+</script>
+
+<style scoped>
+.active {
+  @apply text-white;
+}
+</style>
