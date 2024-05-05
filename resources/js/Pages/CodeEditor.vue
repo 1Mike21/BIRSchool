648b675b1761cf4100005a572b9de8ebdc2d2586
1Mike21 <mihail.sokolov21@yandex.ru>
2024-05-05 19:11:11 +0300

    Added functional editor

--- resources/js/Pages/CodeEditor.vue
+++ resources/js/Pages/CodeEditor.vue
@@ -1,56 +1,70 @@
 <template>
+
   <Head title="Редактор кода" />
 
   <div class="rounded-2xl overflow-hidden">
     <div class="bg-red h-14 flex align-middle px-2">
-      <button title="Очистить"><img class="w-12" src="/img/icons/clear.svg" alt="Очистить"></button>
+      <button @click="clearCode" title="Очистить все"><img class="w-12" src="/img/icons/clear.svg" alt="Очистить"></button>
       <button title="Сохранить"><img class="w-8" src="/img/icons/save.svg" alt="Сохранить"></button>
     </div>
     <div class="code-editor flex justify-between min-h-[800px]">
       <div class="code grid grid-rows-3 overflow-y-auto p-4 bg-white bg-opacity-10 w-[50%]">
-        <div class="html-code">
+        <div>
           <h2 class="title-text"><img src="/img/icons/html-5.svg" alt="HTML" class="w-6 mr-3 inline">HTML</h2>
-          <textarea class="textarea-code"></textarea>
+          <textarea @keyup="run" v-model="editor.html" class="textarea-code"></textarea>
         </div>
-        <div class="css-code">
+        <div>
           <h2 class="title-text"><img src="/img/icons/css-3.svg" alt="CSS" class="w-6 mr-3 inline">CSS</h2>
-          <textarea class="textarea-code"></textarea>
+          <textarea @keyup="run" v-model="editor.css" class="textarea-code"></textarea>
         </div>
-        <div class="js-code">
+        <div>
           <h2 class="title-text"><img src="/img/icons/js.svg" alt="JS" class="w-6 mr-3 inline">JS</h2>
-          <textarea class="textarea-code"></textarea>
+          <textarea @keyup="run" v-model="editor.js" class="textarea-code"></textarea>
         </div>
       </div>
       <div class="bar w-1 bg-slate-400 cursor-w-resize" title="Щелкните и перетащите"></div>
-      <iframe class="bg-white w-[50%]" id="result"></iframe>
+      <iframe class="bg-white w-[50%]" ref="result"></iframe>
     </div>
   </div>
 </template>
 
-<script>
-import { Head, Link } from '@inertiajs/vue3';
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import { onMounted, reactive, ref } from 'vue';
 
-// const html_code = document.querySelector('.html-code textarea');
-// const css_code = document.querySelector('.css-code textarea');
-// const js_code = document.querySelector('.js-code textarea');
-// const result = document.querySelector('#result');
-// const bar = document.querySelector('.bar');
-// const code_editor = document.querySelector('.code-editor');
+const result = ref(null);
 
-// function run() {
-//   localStorage.setItem('html_code', html_code.value);
-//   localStorage.setItem('css_code', css_code.value);
-//   localStorage.setItem('js_code', js_code.value);
-//   result.contentDocument.body.innerHTML = `<style>${localStorage.css_code}</style>` + localStorage.html_code;
-//   result.contentWindow.eval(localStorage.js_code);
-// }
-// html_code.onkeyup = ()=> run();
-// css_code.onkeyup = ()=> run();
-// js_code.onkeyup = ()=> run();
+const editor = reactive({
+  html: '',
+  css: '',
+  js: '',
+});
 
-// html_code.value = localStorage.html_code;
-// css_code.value = localStorage.css_code;
-// js_code.value = localStorage.js_code;
+const clearCode = () => {
+  editor.html = '';
+  editor.css = '';
+  editor.js = '';
+  result.value.contentDocument.body.innerHTML = "";
+}
+
+const run = () => {
+  result.value.contentDocument.body.innerHTML = editor.html;
+  localStorage.setItem('html_code', editor.html);
+  localStorage.setItem('css_code', editor.css);
+  localStorage.setItem('js_code', editor.js);
+  result.value.contentDocument.body.innerHTML = `<style>${localStorage.css_code}</style>` + localStorage.html_code;
+  result.value.contentWindow.eval(localStorage.js_code);
+
+};
+
+onMounted(() => {
+  editor.html = localStorage.html_code;
+  editor.css = localStorage.css_code;
+  editor.js = localStorage.js_code;
+});
+
+// // const bar = document.querySelector('.bar');
+// // const code_editor = document.querySelector('.code-editor');
 </script>
 
 <style scoped>
