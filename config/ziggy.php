6d4368452d0230a1312b70a90eddf9d5b7de55e7
1Mike21 <mihail.sokolov21@yandex.ru>
2024-05-06 18:32:15 +0300

    Added config ziggy

---
+++ config/ziggy.php
@@ -0,0 +1,19 @@
+<?php
+  return [
+    'except' => [
+      'debugbar.*',
+      'ignition.*',
+      '_ignition.*',
+      'unisharp.*',
+      'horizon.*',
+      'private.*',
+      'verification.notice',
+      'verification.verify',
+      'user-profile-information.update',
+      'user-password.update',
+      'password.confirmation',
+      'password.confirm',
+      'sanctum.csrf-cookie',
+    ],
+  ];
+?>
