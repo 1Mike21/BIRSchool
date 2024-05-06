d30dfeae0f386ddb99f86ef3a889335ba027f98e
1Mike21 <mihail.sokolov21@yandex.ru>
2024-05-06 15:05:22 +0300

    Deleted profile dropdown

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -12,104 +12,32 @@
         <Link :href="route('index')">
         <WhiteApplicationLogo class="w-60" />
         </Link>
-        <!-- Settings Options -->
-        <div class="flex justify-between items-center h-14 header-right gap-4">
-          <div v-if="$page.props.auth.user" class="hidden sm:flex sm:items-center ms-1 xl:ms-6">
-            <button type="button"
-              class="relative rounded-full p-1 text-white focus:outline-none focus:ring-2 focus:ring-white">
-              <span class="absolute -inset-1.5" />
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
-            <!-- Profile Dropdown -->
-            <div class="ms-3 relative">
-              <Dropdown align="right" width="48">
-                <template #trigger>
-                  <button v-if="$page.props.jetstream.managesProfilePhotos"
-                    class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                    <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url"
-                      :alt="$page.props.auth.user.name">
-                  </button>
-                  <span v-else class="inline-flex rounded-md">
-                    <button type="button"
-                      class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                      {{ $page.props.auth.user.name }}
-                      <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                      </svg>
-                    </button>
-                  </span>
-                </template>
-                <template #content>
-                  <!-- Account Management -->
-                  <div class="block px-4 py-2 text-xs text-gray-400">
-                    Управление профилем
-                  </div>
-                  <DropdownLink :href="route('profile.show')">
-                    Профиль
-                  </DropdownLink>
-                  <div class="border-t border-gray-200" />
-                  <!-- Authentication -->
-                  <form @submit.prevent="logout">
-                    <DropdownLink as="button">
-                      Выход
-                    </DropdownLink>
-                  </form>
-                </template>
-              </Dropdown>
-            </div>
-          </div>
-          <!-- Hamburger -->
-          <div class="-me-2 flex items-center sm:hidden">
-            <button
-              class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-              @click="showingNavigationDropdown = !showingNavigationDropdown">
-              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path :class="{ 'hidden': showingNavigationDropdown, 'inline-flex': !showingNavigationDropdown }"
-                  stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                <path :class="{ 'hidden': !showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
-                  stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-      <!-- Responsive Navigation Menu -->
-      <div :class="{ 'block': showingNavigationDropdown, 'hidden': !showingNavigationDropdown }" class="sm:hidden">
-        <div v-if="$page.props.auth.user" class="pt-4 pb-1 border-t border-gray-200">
-          <div class="flex flex-col sm:flex-row sm:items-center px-4">
-            <div class="flex justify-between">
-              <div v-if="$page.props.jetstream.managesProfilePhotos" class="shrink-0 me-5">
-                <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url"
-                  :alt="$page.props.auth.user.name">
-              </div>
+        <div class="flex justify-between items-center">
+          <ul v-if="$page.props.auth.user" class="flex items-center justify-between gap-x-5 ms-1 xl:ms-6">
+            <li>
               <button type="button"
-                class="relative mt-2 sm:ml-auto rounded-full p-1 text-white  focus:outline-none focus:ring-2 focus:ring-white">
+                class="relative rounded-full p-1 text-white focus:outline-none focus:ring-2 focus:ring-white">
                 <span class="absolute -inset-1.5" />
                 <span class="sr-only">View notifications</span>
                 <BellIcon class="h-6 w-6" aria-hidden="true" />
               </button>
-            </div>
-            <div class="mt-2">
-              <div class="font-medium text-base text-white">
-                {{ $page.props.auth.user.name }}
-              </div>
-              <div class="font-medium text-sm text-white">
-                {{ $page.props.auth.user.email }}
-              </div>
-            </div>
-          </div>
-          <ul class="mt-3 space-y-1">
-            <ResponsiveNavLink :href="route('profile.show')" :active="route().current('profile.show')">
-              Профиль
-            </ResponsiveNavLink>
-            <!-- Authentication -->
-            <form method="POST" @submit.prevent="logout">
-              <ResponsiveNavLink>
-                Выход
-              </ResponsiveNavLink>
-            </form>
+            </li>
+            <li class="flex flex-col text-right">
+              <span class="text-sm font-medium text-white">{{ $page.props.auth.user.name }}</span>
+              <span class="text-xs font-medium">{{ $page.props.auth.user.role }}</span>
+            </li>
+            <li>
+              <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url"
+                :alt="$page.props.auth.user.name">
+            </li>
+            <li>
+              <Link href="#" class="flex items-center mr-4 hover:text-blue-100">
+                <span class="inline-flex mr-1">
+                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+                </span>
+                Выйти
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
