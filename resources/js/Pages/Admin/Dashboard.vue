49a6f173df6e740d7cd8fbdc350626b7ab710795
1Mike21 <mihail.sokolov21@yandex.ru>
2024-05-15 18:51:19 +0300

    Refactoring routes

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <Head title="Dashboard"/>
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+
+defineOptions({ layout: AdminLayout })
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,32 @@
 <?php
 
+use App\Http\Controllers\ContactsController;
 use App\Http\Controllers\Courses\IndexController as CoursesIndexController;
 use App\Http\Controllers\Courses\ShowController as CoursesShowController;
 use App\Http\Controllers\Groups\ShowController as GroupsShowController;
 use App\Http\Controllers\IndexController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Laravel\Jetstream\Http\Controllers\Inertia\CurrentUserController;
+use Laravel\Jetstream\Http\Controllers\Inertia\OtherBrowserSessionsController;
+use Laravel\Jetstream\Http\Controllers\Inertia\PrivacyPolicyController;
+use Laravel\Jetstream\Http\Controllers\Inertia\ProfilePhotoController;
+use Laravel\Jetstream\Http\Controllers\Inertia\TermsOfServiceController;
+use Laravel\Jetstream\Http\Controllers\Inertia\UserProfileController;
+use Laravel\Jetstream\Jetstream;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$authMiddleware = config('jetstream.guard')
+  ? 'auth:' . config('jetstream.guard')
+  : 'auth';
+
+$authSessionMiddleware = config('jetstream.auth_session', false)
+  ? config('jetstream.auth_session')
+  : null;
+
+// Guest
 Route::get('/', IndexController::class)->name('index');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-
-])->prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
-  Route::get('/users', function () {
-    return Inertia::render('Admin/Users/Index');
-  })->name('users.index');
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
-
-Route::get('/contacts', function () {
-  return Inertia::render('Contacts');
-})->name('contacts');
+Route::get('/contacts', ContactsController::class)->name('contacts');
 
 Route::get('/groups/{group}', GroupsShowController::class)->name('groups.show');
 
@@ -45,7 +34,41 @@ Route::get('/courses', CoursesIndexController::class)->name('courses.index');
 
 Route::get('/courses/{course}', CoursesShowController::class)->name('courses.show');
 
-require_once __DIR__ . '/jetstream.php';
-require_once __DIR__ . '/fortify.php';
+if (Jetstream::hasTermsAndPrivacyPolicyFeature()) {
+  Route::get('/terms-of-service', [TermsOfServiceController::class, 'show'])->name('terms.show');
+  Route::get('/privacy-policy', [PrivacyPolicyController::class, 'show'])->name('policy.show');
+}
 
+Route::group(['middleware' => array_values(array_filter([$authMiddleware, $authSessionMiddleware]))], function () {
+  Route::middleware([
+    'auth:sanctum',
+    'verified',
+    // Admin
+    Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
+      Route::get('/users', function () {
+        return Inertia::render('Admin/Users/Index');
+      })->name('users.index');
+
+      Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+      })->name('dashboard');
+    }),
+
+    // User & Profile...
+    Route::prefix('user')->name('user.')->namespace('App\Http\Controllers\User')->group(function () {
+      Route::get('/profile', [UserProfileController::class, 'show'])->name('profile.show');
+
+      Route::delete('/other-browser-sessions', [OtherBrowserSessionsController::class, 'destroy'])->name('other-browser-sessions.destroy');
+
+      Route::delete('/profile-photo', [ProfilePhotoController::class, 'destroy'])->name('current-user-photo.destroy');
+
+      if (Jetstream::hasAccountDeletionFeatures()) {
+        Route::delete('/', [CurrentUserController::class, 'destroy'])
+        ->name('current-user.destroy');
+      }
+    })
+  ]);
+});
+
+require_once __DIR__ . '/fortify.php';
 ?>
