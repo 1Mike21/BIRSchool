2704d0f64eefa270da0feb3926efb25e2e0590cd
1Mike21 <mihail.sokolov21@yandex.ru>
2024-05-17 13:22:46 +0300

    Fixed show pages profile and my courses

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Laravel\Jetstream\Http\Controllers\Inertia\OtherBrowserSessionsController;
 use Laravel\Jetstream\Http\Controllers\Inertia\PrivacyPolicyController;
 use Laravel\Jetstream\Http\Controllers\Inertia\ProfilePhotoController;
 use Laravel\Jetstream\Http\Controllers\Inertia\TermsOfServiceController;
-use Laravel\Jetstream\Http\Controllers\Inertia\UserProfileController;
+use App\Http\Controllers\User\UserProfileController;
 use Laravel\Jetstream\Jetstream;
 
 $authMiddleware = config('jetstream.guard')
