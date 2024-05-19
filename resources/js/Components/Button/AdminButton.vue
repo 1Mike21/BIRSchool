313a8bbfeaec8d239e3fcdb5c6427c31d5f8c350
1Mike21 <mihail.sokolov21@yandex.ru>
2024-05-19 16:35:08 +0300

    Changed Admin form

--- resources/js/Components/Button/AdminButton.vue
+++ resources/js/Components/Button/AdminButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button v-if="as == 'button'"
-    :class="['bg-violetButton min-w-16 hover:bg-violetHoverButton focus:ring-4 focus:ring-gray-300 rounded-lg text-white font-medium text-sm mr-3 justify-center px-3 py-2 sm:w-auto md:text-base text-center', { 'inline-flex items-center': hasIcon }]"
+    :class="['bg-violetButton min-w-16 hover:bg-violetHoverButton focus:ring-4 focus:ring-gray-300 rounded-lg text-white font-medium text-sm mr-3 justify-center px-3 py-2 sm:w-auto md:text-base text-center', { 'inline-flex items-center': hasIcon }, { 'opacity-75': submitted }]"
     :type="type">
     <slot />
   </button>
@@ -23,6 +23,10 @@ defineProps({
   type: {
     type: String,
     default: 'button',
+  },
+  submitted: {
+    type: Boolean,
+    default: false
   },
   hasIcon: {
     type: Boolean,
