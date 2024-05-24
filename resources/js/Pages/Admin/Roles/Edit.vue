4c30236db8050ab6e9c8a7fe6bb5bfa8a32eb0a8
1Mike21 <mihail.sokolov21@yandex.ru>
2024-05-24 21:22:44 +0300

    Added permission for CRUD Role

--- resources/js/Pages/Admin/Roles/Edit.vue
+++ resources/js/Pages/Admin/Roles/Edit.vue
@@ -2,7 +2,7 @@
 
   <Head title="Редактирование роли" />
 
-  <ModalPage max-width="lg">
+  <ModalPage max-width="md">
     <template #title>
       <h3 class="text-xl font-semibold">
         Редактирование роли
@@ -10,12 +10,17 @@
     </template>
     <template #content>
       <AdminForm id="update_role_form" @submitted="form.put(route('admin.roles.update', role.id))">
-        <AdminFormGroup>
+        <AdminFormGroup class="!col-span-12">
           <InputLabel class="mb-2" for="name" value="Название роли" />
           <TextInput v-model="form.name" id="name"
             :class="['bg-gray-50 border text-black sm:text-sm block w-full p-2.5', { 'border-red': form.errors.name }]"
             placeholder="Введите название роли" type="text" autofocus />
           <InputError class="mt-2" :message="form.errors.name" />
+        </AdminFormGroup>
+        <AdminFormGroup class="!col-span-12">
+          <InputLabel class="mb-2" for="permissions" value="Разрешения" />
+          <MultiSelect v-model="form.permissions" :options="permissions" id="permissions" :show-labels="false"
+            label="name" :multiple="true" :close-on-select="false" track-by="id" placeholder="Выберите разрешения" />
         </AdminFormGroup>
       </AdminForm>
     </template>
@@ -37,15 +42,24 @@ import TextInput from '@/Components/FormElement/TextInput.vue';
 import InputError from '@/Components/FormElement/InputError.vue';
 import ModalPage from '@/Components/Modal/ModalPage.vue';
 import { useForm } from '@inertiajs/vue3';
+import { onMounted } from 'vue';
 
 const props = defineProps({
   role: {
     type: Object,
     required: true
+  },
+  permissions: {
+    type: Array
   }
+});
+
+onMounted(() => {
+  form.permissions = props.role?.permissions;
 });
 
 const form = useForm({
   name: props.role.name,
+  permissions: [],
 });
 </script>
