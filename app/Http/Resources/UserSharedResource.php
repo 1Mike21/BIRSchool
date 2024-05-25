4d318e3a3b992ca7f001ed4b04d38b7126d28ab8
1Mike21 <mihail.sokolov21@yandex.ru>
2024-05-25 15:21:53 +0300

    Added UserSharedResource

---
+++ app/Http/Resources/UserSharedResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserSharedResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+      return [
+        'id' => $this->id,
+        'name' => $this->name,
+        'surname' => $this->surname,
+        'profile_photo_url' => $this->profile_photo_url,
+        'phone_number' => $this->phone_number,
+        'email' => $this->email,
+        'roles' => $this->getRoleNames(),
+        'permissions' => $this->getPermissionNames(),
+        'created_at' => $this->created_at->format('d.m.Y'),
+      ];
+    }
+}
